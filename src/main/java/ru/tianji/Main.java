d9e0b36cd348f4a5007476b122bde84d4106e6d1
Laodzhan79 <wakmakov@gmail.com>
2026-07-20 18:16:03 +0300

    v0.0.1 Heartbeat build system

---
+++ src/main/java/ru/tianji/Main.java
@@ -0,0 +1,14 @@
+package ru.tianji;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" TianJi v0.0.1 Heartbeat");
+        System.out.println("=================================");
+        System.out.println("Core started.");
+
+    }
+
+}
